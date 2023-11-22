0f98059559728f2df3fd62ff4bfd2ac96353b7ed
Barrett Langton <barrett@langton.dev>
2023-11-21 19:44:12 -0700

    Create Western States 100 lottery Monte Carlo simulations

---
+++ README.md
@@ -0,0 +1,41 @@
+# Monte Carlo simulation for the Western States 100
+
+See my [Hardrock simulation
+here](https://github.com/bdlangton/monte-carlo-for-hardrock)
+
+Run simulations to calculate your odds of getting into the lottery. This is the
+same method used by Western States
+[themselves](https://www.wser.org/2022/12/02/2023-lottery-statistics/) but with
+this you can run the odds before Western States officially publishes the
+results.
+
+Note that there could be differences between these results and what Western
+States publishes. It depends on how many simulation runs you do, but also it depends on
+if the total number of selections changes and whether the preliminary ticket
+numbers end up changing.
+
+This does not factor in your odds of getting onto the waitlist, just your odds
+of getting selected into the original list of starters.
+
+## Running simulations
+
+You can run simulations by invoking `main.rb` and it'll prompt you for how many
+simulations to run. Then it will output the average number of people selected
+per category as well as the odds for each category.
+
+```
+ruby main.rb
+```
+
+You could also start up `irb` and run it that way:
+
+```
+load 'monte_carlo.rb'
+mc = MonteCarlo.new(<num-of-simulations>)
+```
+
+## Entrants
+
+Entrant counts are hardcoded in `monte_carlo.rb` so make sure those are updated
+before running simulations. Previous year values are on [tagged
+commits](https://github.com/bdlangton/monte-carlo-for-western-states/tags).
---
+++ monte_carlo.rb
@@ -0,0 +1,100 @@
+class MonteCarlo
+  attr_reader :entrants
+  attr_reader :selected_entrants
+  attr_reader :averages
+  attr_reader :odds
+
+  def initialize(simulations = 1000)
+    @simulations = simulations
+    @total_picks = 264
+
+    @entrants = {
+      1 => 3560,
+      2 => 1578,
+      4 => 731,
+      8 => 525,
+      16 => 374,
+      32 => 232,
+      64 => 127,
+      128 => 37,
+      256 => 5,
+    }
+
+    set_all_tickets
+
+    # Stores sum of all selected entrants for all simulations
+    @selected_entrants = new_entrants_list
+  end
+
+  def run_simulations
+    puts "Running #{@simulations} simulations"
+
+    @simulations.times do
+      tickets_left = Marshal.load(Marshal.dump(@all_tickets))
+      picks_left = @total_picks
+
+      while picks_left > 0 do
+        # Select a random ticket
+        num_to_remove = tickets_left[tickets_left.keys.sample]
+        @selected_entrants[num_to_remove] += 1
+
+        # Remove appropriate tickets from pool
+        removals = 0
+        tickets_left.each do |ticket_no, ticket_type|
+          if ticket_type == num_to_remove
+            removals += 1
+            tickets_left.delete(ticket_no)
+            break if removals >= num_to_remove
+          end
+        end
+
+        picks_left -= 1
+      end
+    end
+  end
+
+  def calculate_odds
+    calculate_averages
+
+    @odds = {}
+    @entrants.each do |key, val|
+      @odds[key] = (100 * @averages[key] / val).round(2)
+    end
+
+    @odds
+  end
+
+  private
+
+  def calculate_averages
+    @averages = new_entrants_list(true)
+
+    @selected_entrants.each do |key, val|
+      @averages[key] = val.to_f / @simulations
+    end
+
+    @averages
+  end
+
+  def set_all_tickets
+    cnt = 0
+    @all_tickets = {}
+
+    # Add each individual ticket to the pool
+    @entrants.each do |key, num|
+      num_tickets = num * key
+      num_tickets.times do
+        @all_tickets.merge!({cnt => key})
+        cnt += 1
+      end
+    end
+  end
+
+  def new_entrants_list(as_double = false)
+    entrants_list = {}
+    @entrants.each do |key, _|
+      entrants_list[key] = as_double ? 0.0 : 0
+    end
+    entrants_list
+  end
+end
